0078c29f617163efe74aa4c6e7ed027b61c85aaf
Hannah Cushman <hannah.cushman@gmail.com>
2017-01-10 13:17:51 -0600

    generate csv rather than geojson from retrieve_census.py

--- data/police_districts.mk
+++ data/police_districts.mk
@@ -1,2 +1,3 @@
-output/police_district_population.geojson : raw/police_district_boundaries.geojson
-	python scripts/retrieve_census.py > $@
+output/police_district_demographics.csv : raw/police_district_boundaries.geojson
+	(echo dist_num,black_pop,total_pop,pct_black; \
+	 python scripts/retrieve_census.py) > $@
--- data/scripts/retrieve_census.py
+++ data/scripts/retrieve_census.py
@@ -1,11 +1,14 @@
 # acs5 docs: view-source:http://api.census.gov/data/2015/acs5/variables.html
 
+import csv
 import json
 import sys
 
 from census_area import Census
 
-c = Census("ce143e4c445f1ccaed3aa214715bebcb9a487cfa")
+from config import CENSUS_API_KEY
+
+c = Census(CENSUS_API_KEY)
 
 black_alone_total = "B02001_003E"
 all_total = "B02001_001E"
@@ -14,18 +17,19 @@ with open("raw/police_district_boundaries.geojson", "r") as f:
     geojson = f.read()
     police_districts = json.loads(geojson)
 
-features = []
+writer = csv.writer(sys.stdout)
 
 for district in police_districts["features"]:
-    if district["properties"]["dist_num"] == "31":
+    dist_num = district["properties"]["dist_num"]
+    # omit norridge, harwood heights, and mt. greenwood cemetery
+    if dist_num == "31":
         continue
-    district["properties"].update({"black_population": 0, "total_population": 0})
+    dist_data = [dist_num, 0, 0]
     black_pop = c.acs5.geo_tract(("NAME", black_alone_total), district["geometry"])
     for _, tract_data in black_pop:
-        district["properties"]["black_population"] += int(tract_data[black_alone_total])
+        dist_data[1] += int(tract_data[black_alone_total])
     total_pop = c.acs5.geo_tract(("NAME", all_total), district["geometry"])
     for _, tract_data in total_pop:
-        district["properties"]["total_population"] += int(tract_data[all_total])
-    features.append(district)
-
-sys.stdout.write(json.dumps({"type": "FeatureCollection", "features": features}, indent=4))
+        dist_data[2] += int(tract_data[all_total])
+    dist_data.append(round(dist_data[1]/dist_data[2]*100, 2))
+    writer.writerow(dist_data)
