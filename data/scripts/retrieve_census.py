2c59cff3996973455a6fc939da363752e5689345
Hannah Cushman <hannah.cushman@gmail.com>
2017-01-10 14:01:57 -0600

    uses block group rather than census tract pop estimates

--- data/scripts/retrieve_census.py
+++ data/scripts/retrieve_census.py
@@ -1,4 +1,4 @@
-# acs5 docs: view-source:http://api.census.gov/data/2015/acs5/variables.html
+# acs5 docs: http://api.census.gov/data/2015/acs5/variables.html
 
 import csv
 import json
@@ -14,22 +14,21 @@ black_alone_total = "B02001_003E"
 all_total = "B02001_001E"
 
 with open("raw/police_district_boundaries.geojson", "r") as f:
-    geojson = f.read()
-    police_districts = json.loads(geojson)
+    police_districts = json.load(f)
 
 writer = csv.writer(sys.stdout)
 
 for district in police_districts["features"]:
     dist_num = district["properties"]["dist_num"]
-    # omit norridge, harwood heights, and mt. greenwood cemetery
-    if dist_num == "31":
+    if dist_num == "31": # omit norridge, harwood heights, and mt. greenwood cemetery
         continue
     dist_data = [dist_num, 0, 0]
-    black_pop = c.acs5.geo_tract(("NAME", black_alone_total), district["geometry"])
-    for _, tract_data in black_pop:
-        dist_data[1] += int(tract_data[black_alone_total])
-    total_pop = c.acs5.geo_tract(("NAME", all_total), district["geometry"])
-    for _, tract_data in total_pop:
-        dist_data[2] += int(tract_data[all_total])
-    dist_data.append(round(dist_data[1]/dist_data[2]*100, 2))
+    black_pop = c.acs5.geo_blockgroup(("NAME", black_alone_total), district["geometry"])
+    for _, blockgroup_data in black_pop:
+        dist_data[1] += int(blockgroup_data[black_alone_total])
+    total_pop = c.acs5.geo_blockgroup(("NAME", all_total), district["geometry"])
+    for _, blockgroup_data in total_pop:
+        dist_data[2] += int(blockgroup_data[all_total])
+    pct_black = round(dist_data[1]/dist_data[2]*100, 2)
+    dist_data.append(pct_black)
     writer.writerow(dist_data)
