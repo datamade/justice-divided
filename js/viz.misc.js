eddb374157cd017d0c3d8f8a2fc4c603f35f0f62
Hannah Cushman <hannah.cushman@gmail.com>
2017-03-31 14:15:13 -0500

    add missing js/css

---
+++ js/viz.misc.js
@@ -0,0 +1,41 @@
+// dynamically set height of sections
+$('.section').each(function() {
+    $(this).css({'min-height': window.innerHeight * 1.25});
+})
+
+// animate header
+var anim = function() {
+  $(document).one('scroll', function(event) {
+    $('div#header').css({'animation': 'play 1s steps(14)',
+                         'animation-fill-mode': 'forwards'});
+  }
+)}
+
+$('div#header').on('inview', function(event, isInView) {
+  if (isInView) { setTimeout(anim, 250) }
+  else { $(this).css('animation', '') }
+})
+
+// stats slides
+$('div#stages').slick({
+  prevArrow: '<i class="fa fa-chevron-left fa-fw pull-left"></i>',
+  nextArrow: '<i class="fa fa-chevron-right fa-fw pull-right"></i>',
+  appendArrows: $('div#stages-nav'),
+  dots: true,
+  appendDots: $('div#status')
+});
+
+// charts
+new Chartist.Pie('#youth_demographics', {
+  series: [40, 60]}, {
+    startAngle: 270,
+    total: 100,
+    showLabel: false
+  });  
+
+new Chartist.Pie('#arrest_demographics', {
+  series: [79, 21]}, {
+    startAngle: 270,
+    total: 100,
+    showLabel: false
+});
