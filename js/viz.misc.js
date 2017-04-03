73d75613105a2794358efbcfd02d9c7451e8b7d9
Hannah Cushman <hannah.cushman@gmail.com>
2017-04-03 10:18:21 -0500

    add action page, extend learn page

--- js/viz.misc.js
+++ js/viz.misc.js
@@ -1,8 +1,3 @@
-// dynamically set height of sections
-$('.section').each(function() {
-    $(this).css({'min-height': window.innerHeight * 1.25});
-})
-
 // animate header
 var anim = function() {
   $(document).one('scroll', function(event) {
@@ -15,12 +10,3 @@ $('div#header').on('inview', function(event, isInView) {
   if (isInView) { setTimeout(anim, 250) }
   else { $(this).css('animation', '') }
 })
-
-// stats slides
-$('div#stages').slick({
-  prevArrow: '<i class="fa fa-chevron-left fa-fw pull-left"></i>',
-  nextArrow: '<i class="fa fa-chevron-right fa-fw pull-right"></i>',
-  appendArrows: $('div#stages-nav'),
-  dots: true,
-  appendDots: $('div#status')
-});
