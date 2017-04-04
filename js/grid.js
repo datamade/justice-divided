a91a19a52ca352b5d997010541655825f53838b1
Hannah Cushman <hannah.cushman@gmail.com>
2017-04-04 13:31:44 -0500

    add semicolons to grid.js

--- js/grid.js
+++ js/grid.js
@@ -1,26 +1,28 @@
 var makeGrid = function(id, total, num_per_row, subpop1 = 0, subpop2 = 0) { 
-  selector = 'div.grid#' + id
+  selector = 'div.grid#' + id;
   
-  $(selector).append('<span class="red"></span>')
+  $(selector).append('<span class="red"></span>');
   
   // if this isn't the arrests grid
-  if (id.split('_')[0] != 'arrests') {
-    $(selector).append('<span class="gray"></span>')
+  if ( id.split('_')[0] != 'arrests' ) {
+    $(selector).append('<span class="gray"></span>');
   }
 
-  for (i = 0; i < total; i++) {
+  for ( i = 0; i < total; i++ ) {
 
     if ( i < subpop1 || !Boolean(subpop1) ) { 
-      el = $(selector +' span:first-child') 
+      el = $(selector +' span:first-child'); 
     } else if ( i < subpop2 || !Boolean(subpop2) ) { 
-      el = $(selector + ' span:nth-child(2)') 
+      el = $(selector + ' span:nth-child(2)');
     } else { 
-      el = $(selector) 
+      el = $(selector);
     }
 
-    if (Boolean(i) & !(i % num_per_row)) { el.append('<br>') }
+    if ( Boolean(i) & !(i % num_per_row) ) { 
+      el.append('<br>');
+    }
 
-    el.append('<i class="fa fa-male fa-fw" aria-hidden="true"></i>') 
+    el.append('<i class="fa fa-male fa-fw" aria-hidden="true"></i>');
   }
 }
 
