e888fc359182047e82f66bda7145c6754b18767c
Hannah Cushman <hannah.cushman@gmail.com>
2017-04-05 21:47:04 -0500

    js reorg + google geocoder

--- js/district_map.js
+++ js/district_map.js
@@ -1,3 +1,4 @@
+// define map
 var districtMap = L.map('district-map').setView([41.8781, -87.6298], 10);
 
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/{style}/{z}/{x}/{y}.png', {
@@ -10,19 +11,36 @@ var districtLayer = L.geoJson()
 districtLayer.addTo(districtMap)
 
 // http://api.jquery.com/jquery.getjson/
-districts = getJSON('data/raw/cpd_district_boundaries.geojson', function(data) {
-    if ( data.properties.dist_num != 31 ) {
-        districtLayer.addData(data);
-        // set display: none
-    }
+districts = $.getJSON('data/raw/cpd_district_boundaries.geojson', function(data) {
+// if ( data.properties.dist_num != 31 ) {
+    // districtLayer.addData(data);
+    // set display: none
+    $.noop();
 }).done($.noop()); // load geojson into a variable that i can reference later
 
-$('#search-district').click(function() {
-    inp = $(this).parent().prev();
-    address = encodeURI(inp.val());
-    queryString = `http://nominatim.openstreetmap.org/search/?q=${address},%20chicago,%20illinois&format=json`;
-    // get lat/long
-    // inDistrict = leafletPip.pointInLayer([long, lat], districtLayer);
-    // inDistrict.css('visibility', 'visible');
-    // render stuff in template
-})
+// define interactivity
+function initGeocoder() {
+    var geocoder = new google.maps.Geocoder;
+
+    $('#search-district').click(function() {
+        inp = $(this).parent().prev();
+        address = encodeURI(inp.val());
+        retrieveCoordinates(geocoder, address);
+    })
+}
+
+function retrieveCoordinates(geocoder, address) {
+    geocoder.geocode({'address': address + 'chicago, il'}, function(results, status) {
+        if (status === 'OK') {
+            coord = results[0].geometry.location;
+            [lat, lng] = [coord.lat(), coord.lng()];
+            // inDistrict = leafletPip.pointInLayer([long, lat], districtLayer, first=true);
+            // inDistrict.css('visibility', 'visible');
+            // render stuff in template
+        } else {
+          console.log(':-(');
+        }
+    });
+}
+
+
