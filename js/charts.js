3dd5b2d1219aa4dd640ad03b1e19c4edc7d24c10
Hannah Cushman <hancush@users.noreply.github.com>
2017-05-01 16:21:10 -0500

    Rough draft v1 (Grid visualization) (#10)
    - Dynamically generate people grids with JavaScript
    - Extend text content
    - Implement assorted charts with Chartist.js
    - Add and populate "Action" page

---
+++ js/charts.js
@@ -0,0 +1,151 @@
+function generateLabels(x) {
+  if ( !(x % 5) ) {
+    return x + '%'; 
+  } else {
+    return '';
+  }
+}
+
+// define charts
+var marijuanaArrestsChart = new Chartist.Bar('.ct-chart#marijuana_arrests', {
+  labels: [
+    'Juvenile Population',
+    'Marijuana Arrests'
+  ],
+  series: [
+    [39, 71],
+    [36, 23],
+  ]
+}, {
+  high: 100,
+  width: '100%',
+  height: '40vh',
+  seriesBarDistance: 30,
+  horizontalBars: true,
+  reverseData: true,
+  axisY: {
+    showGrid: false,
+  },
+  axisX: {
+    labelInterpolationFnc: generateLabels
+  }
+});
+
+var allArrestsChart = new Chartist.Bar('#all_arrests', {
+  labels: [
+    'Juvenile Population',
+    'All Arrests'
+  ],
+  series: [
+    [39, 79],
+    [36, 3],
+  ]
+}, {
+  high: 100,
+  width: '100%',
+  height: '40vh',
+  seriesBarDistance: 30,
+  horizontalBars: true,
+  reverseData: true,
+  axisY: {
+    showGrid: false,
+  },
+  axisX: {
+    labelInterpolationFnc: generateLabels
+  }
+});
+
+var systemDisparityChart = new Chartist.Bar('#system_disparity', {
+  labels: [
+    'Detention', 
+    'Probation', 
+    'Incarceration'
+  ],
+  series: [
+    [80.6, 85.6, 82.5],
+    [15.6, 7.6, 3.5],
+    [3.5, 5.6, 2.1]
+  ]
+}, {
+  high: 100,
+  width: '100%',
+  height: '65vh',
+  seriesBarDistance: 30,
+  horizontalBars: true,
+  reverseData: true,
+  axisY: {
+    showGrid: false,
+  },
+  axisX: {
+    labelInterpolationFnc: generateLabels
+  }
+});
+
+var educationChart = new Chartist.Bar('#education_disparity', {
+  labels: [
+    'Attend College', 
+    'Graduate High School' 
+  ],
+  series: [
+    [27, 49],
+    [18, 34]
+  ]
+}, {
+  high: 75,
+  width: '100%',
+  height: '40vh',
+  horizontalBars: true,
+  seriesBarDistance: 30,
+  axisY: {
+    showGrid: false,
+  },
+  axisX: {
+    labelInterpolationFnc: generateLabels
+  }
+});
+
+
+// label bars
+
+function makeRaceLabels(chart) {
+  chart.on('draw', function(data) {
+    if ( data.type === 'bar' ) {
+      labels = ['white', 'Hispanic', 'black'];
+      if ( data.series.length == 2 ) {
+        labels.splice(1, 1); // remove Hispanic (to-do: get this population)
+      }
+      baseClass = 'ct-bar-label';
+      if ( data.seriesIndex < data.series.length - 1 ) {
+        baseClass += ' mute'; // mute non-black labels
+      } 
+      data.group.elem('text', {
+        x: data.x2 + 10,
+        y: data.y1 + 5,
+      }, baseClass).text(
+        data.value.x + '% ' + labels[data.seriesIndex]
+      );
+    }
+  });
+}
+
+var raceCharts = [marijuanaArrestsChart, allArrestsChart, systemDisparityChart];
+
+raceCharts.forEach(function(chart) {
+  makeRaceLabels(chart);
+})
+
+function makeChanceLabels(chart) {
+  chart.on('draw', function(data) {
+    if ( data.type === 'bar' ) {
+      baseClass = 'ct-bar-label';
+      data.group.elem('text', {
+        x: data.x2 + 10,
+        y: data.y1 + 5,
+      }, baseClass).text(
+        data.value.x + '% chance'
+      );
+    }
+  });  
+}
+
+makeChanceLabels(educationChart);
---
+++ js/grid.js
@@ -0,0 +1,34 @@
+var makeGrid = function(id, total, num_per_row, subpop1 = 0, subpop2 = 0) { 
+  selector = 'div.grid#' + id;
+  
+  $(selector).append('<span class="red"></span>');
+  
+  // if this isn't the arrests grid
+  if ( id.split('_')[0] != 'arrests' ) {
+    $(selector).append('<span class="gray"></span>');
+  }
+
+  for ( i = 0; i < total; i++ ) {
+
+    if ( i < subpop1 || !Boolean(subpop1) ) { 
+      el = $(selector +' span:first-child'); 
+    } else if ( i < subpop2 || !Boolean(subpop2) ) { 
+      el = $(selector + ' span:nth-child(2)');
+    } else { 
+      el = $(selector);
+    }
+
+    if ( Boolean(i) & !(i % num_per_row) ) { 
+      el.append('<br>');
+    }
+
+    el.append('<i class="fa fa-male fa-fw" aria-hidden="true"></i>');
+  }
+}
+
+makeGrid('base_pop_a', 3250, 65, 15, 650);
+makeGrid('base_pop_b', 3250, 65, 1, 650);
+makeGrid('marijuana_a', 650, 25, 15);
+makeGrid('marijuana_b', 650, 25, 1);
+makeGrid('arrests_a', 15, 5);
+makeGrid('arrests_b', 1, 1);
